027f6f6aa2093cb01b09d4d6f36b803de67df2bb
Peyman Mortazavi <pey.mortazavi@gmail.com>
2015-07-09 12:16:46 -0700

    added android builder

--- src/builders/android.js
+++ src/builders/android.js
@@ -5,7 +5,10 @@ var exec = require('sync-exec');          // Exec utilities
 var format = require('string-format');    // String formatting
 var uuid = require('node-uuid');          // Unique id generator.
 var os = require('os');
-var xml2js = require('xml2js');     // XML Parser
+var async = require('asyncawait/async');
+var await = require('asyncawait/await');
+var Promise = require("bluebird");
+var xml2js = Promise.promisifyAll(require("xml2js"));
 
 format.extend(String.prototype);
 
@@ -38,7 +41,7 @@ module.exports = function(monkey) {
   this.projectRootPath = path.join(this.solutionRootPath, this.options.android.projectName.value);
 }
 
-module.exports.prototype.installConfig = function (configName) {
+module.exports.prototype.installConfig = async (function (configName) {
 
   var nameValuePair = {};
 
@@ -70,14 +73,157 @@ module.exports.prototype.installConfig = function (configName) {
 
   var parser = new xml2js.Parser();
   var builder = new xml2js.Builder();
-  // Step3: Manipulate Info.plist
+
+  // Step3: Manipulate AndroidManifest.xml
   try {
-    var manifestPath = path.join(this.projectRootPath, 'Properties', 'AndroidManifest.xml');
-    var manifestXmlFile = fs.readFileSync(manifestPath, 'utf8');
+    var appConfig = configurationObject.app;
+    if(configurationObject.app) {
 
+      var manifestPath = path.join(this.projectRootPath, 'Properties', 'AndroidManifest.xml');
+      var manifestXmlFile = fs.readFileSync(manifestPath, 'utf8');
 
+      var manifestData = await(parser.parseStringAsync(manifestXmlFile));
+      if(appConfig.name && appConfig.name.value) {
+        manifestData['manifest']['application'][0]['$']['android:label'] = appConfig.name.value;
+        nameValuePair[appConfig.name.name||'name'];
+      }
+      if(appConfig.bundleId && appConfig.bundleId.value) {
+        manifestData['manifest']['$']['package'] = appConfig.bundleId.value;
+        nameValuePair[appConfig.bundleId.name||'bundleId'];
+      }
+      if(appConfig.version && appConfig.version.value) {
+        manifestData['manifest']['$']['android:versionCode'] = appConfig.version.value;
+        nameValuePair[appConfig.version.name||'version'];
+      }
+      if(appConfig.versionName && appConfig.versionName.value) {
+        manifestData['manifest']['$']['android:versionName'] = appConfig.versionName.value;
+        nameValuePair[appConfig.versionName.name||'versionName'];
+      }
+      var newManifestXmlFile = builder.buildObject(manifestData);
+      fs.writeFileSync(manifestPath, newManifestXmlFile);
+    }
   } catch (exception) {
-    throw { innerException: exception, message: "Could not update Info.plist" };
+    throw { innerException: exception, message: "Could not update AndroidManifest.xml" };
   }
 
+  // Step4: Manipulate settings.xml
+  try {
+    var settingsPath = path.join(this.projectRootPath,
+      this.options.android.resourcesPath.value, 'values', 'settings.xml');
+    var settingsXmlFile = fs.readFileSync(settingsPath, 'utf8');
+
+    var settingsData = await(parser.parseStringAsync(settingsXmlFile));
+    saveConfigObject(configurationObject.config, settingsData, nameValuePair);
+  } catch (exception) {
+    throw { innerException: exception, message: "Could not update settings.xml" };
+  }
+
+  // Step5: Copy resources if any
+  try {
+    var projectResourcesPath = resolvePath(this.projectRootPath, this.options.android.resourcesPath.value);
+    var configResourcesPath = path.join(resolvePath(this.solutionRootPath, this.options.project.configsPath.value), configName, 'android.resources');
+    if(fs.existsSync(projectResourcesPath) && fs.existsSync(configResourcesPath)) {
+      fs.copySync(configResourcesPath, projectResourcesPath);
+    }
+  } catch (exception) {
+    throw { innerException: exception, message: "Could not install resources for config: " + configName};
+  }
+
+  return { installedConfigName: configName, configs: nameValuePair };
+
+});
+
+module.exports.prototype.build = function(target, outputPath) {
+
+  var target = target || 'Release'; // default target is release.
+  var buildResults = {};
+
+  if(target == 'AppStore') target == 'Release'; // Release is used even for Store
+
+  try {
+
+    var parser = new xml2js.Parser();
+
+    var buildId = uuid.v4();
+    var tempDir = path.join(os.tmpdir(), 'com.monkeymaker.Builder', 'Android.' + buildId);
+
+    // Clean it first.
+    exec('xbuild {0} /p:Configuration={1} /t:Clean'
+        .format(path.join(this.projectRootPath, this.options.android.projectName.value+".csproj"),
+            target.replace(' ', '-')));
+
+    // Build it.
+    var execResult = exec('xbuild {0} /p:Configuration={1} /t:SignAndroidPackage /p:OutputPath="{2}/"'
+        .format(path.join(this.projectRootPath, this.options.android.projectName.value+".csproj"),
+            target.replace(' ', '-'), tempDir));
+
+    buildResults.stdout = execResult.stdout;
+    buildResults.success = execResult.status == 0;
+
+    var filesList = fs.readdirSync(srcpath).filter(function(file) {
+      return (/-Signed[.]apk$/.exec(file) != null);
+    });
+
+    if(buildResults.success && filesList.length > 0) {
+      var packageUrl = path.join(tempDir, filesList[0]);
+      outputPath = outputPath || path.join(this.projectRootPath, 'bin', target);
+      fs.mkdirsSync(outputPath);
+      var outputUrl = path.join(outputPath, packageName);
+      fs.copySync(packageUrl, outputUrl);
+      buildResults.outputUrl = outputUrl;
+    } else {
+      buildResults.success = false;
+      buildResults.message = "xbuild returns success but no apk was produced.";
+    }
+
+  } catch (exception) {
+    buildResults.success = false;
+    buildResults.exception = exception;
+    buildResults.message = "Build failed.";
+    if(execResult) buildResults.stderr = execResult.stderr;
+  } finally {
+    // Clean it.
+    if(tempDir) {
+      fs.removeSync(tempDir);
+    }
+  }
+  return buildResults;
+}
+
+function saveConfigObject(configObject, settings, nameValuePair) {
+  if(!configObject) return;
+  for (var key in configObject) {
+    var valueDetails = configObject[key];
+    if(valueDetails && valueDetails.value) {
+      var type = typeof(valueDetails.value) == 'boolean' ? 'bool' : 'string';
+      setConfig(settings['resources'][type], valueDetails.key||key, valueDetails.value);
+      if(valueDetails.name) nameValuePair[valueDetails.name] = valueDetails.value;
+    } else { // if there is no value given, it is an object with sub-properties (probably :D).
+      saveConfigObject(configObject[key], plistPath);
+    }
+  }
+}
+
+function setConfig(data, key, value) {
+  for(var i = 0; i < data.length; i++) {
+    if(data[i]['$']['name'] === key) {
+      data[i]['_'] = value;
+      return;
+    }
+  }
+}
+
+function appendObject(object1, object2) {
+  if(!object1) return object2;
+  for (var key in object2) {
+    if (!object1.hasOwnProperty(key)) {
+      object1[key] = object2[key];
+    }
+  }
+  return object1;
+}
+
+function resolvePath(prefix, directoryPath) {
+  if(path.isAbsolute(directoryPath)) return directoryPath;
+  return path.join(prefix, directoryPath);
 }
